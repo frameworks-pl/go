b7987006c056bdd747295b5be5ed87c2e5fdb7ff
Janusz Grabis <janusz@frameworks.pl>
2023-06-21 04:48:54 +0200

    Hello world turned into http one

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
-package main;
+package main
 
 import "fmt"
+import "net/http"
+import "log"
 
 func main() {
-	fmt.Println("hello world")
+
+    http.HandleFunc("/helloworld", func(w http.ResponseWriter, r *http.Request){
+        fmt.Fprintf(w, "Hello, World!")
+    })
+
+    fmt.Printf("Server running (port=8080), route: http://localhost:8080/helloworld\n")
+    if err := http.ListenAndServe(":8081", nil); err != nil {
+        log.Fatal(err)
+    }
 }
